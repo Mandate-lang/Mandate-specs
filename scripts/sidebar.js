34d0296dccd2a437553dd96b9a2ed25ce67fa378
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-07-29 16:47:21 -0400

    Fix typo

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,7 +1,7 @@
 async function load_sidebar()
 {
     const file = await fetch("pages/sidebar.html");
-    const html = html.text();
+    const html = file.text();
 
     //There should be only one body element
     document.getElementsByName("body")[0].insertAdjacentHTML("afterbegin", html);
