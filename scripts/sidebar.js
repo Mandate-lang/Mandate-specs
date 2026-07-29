0602f99ebba11863c900fa79ce658a10c3a1e66e
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-07-29 16:43:43 -0400

    Add sidebar importable with js

---
+++ scripts/sidebar.js
@@ -0,0 +1,13 @@
+async function load_sidebar()
+{
+    const file = await fetch("pages/sidebar.html");
+    const html = html.text();
+
+    //There should be only one body element
+    document.getElementsByName("body")[0].insertAdjacentHTML("afterbegin", html);
+}
+
+document.addEventListener("DOMContentLoaded", function() 
+{
+  load_sidebar();
+});
