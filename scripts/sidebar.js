0874175de100dca30d519cb8eaa5099c4c78ed2c
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-08-10 16:57:30 -0400

    Make sidebar options change based on what page you are on

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -8,6 +8,12 @@ function set_caps(name)
 {
   return String(name).charAt(0).toUpperCase() + String(name).slice(1);
 }
+function get_page()
+{
+  const path = window.location.pathname;
+  const page_name = path.split("/").pop();
+  return page_name;
+}
 
 
 // Functions for adding sidebar
@@ -36,11 +42,18 @@ async function load_sidebar()
 }
 async function setup_table_of_contents()
 {
+  const current_page = get_page();
+  let path_level = "../";
+  if (current_page == "index.html")
+  {
+    path_level = "";
+  }
+
   const table_of_contents = await document.getElementById("table-of-contents");
   for (const page of pages)
   {
     const link = "<li>\n" +
-                 "  <a href=\"../pages/" + page + ".html\">" + set_caps(page) +"</a>\n" +
+                 "  <a href=\"" + path_level + "pages/" + page + ".html\">" + set_caps(page) +"</a>\n" +
                  "</li>"
     
     table_of_contents.insertAdjacentHTML("beforeend", link);
