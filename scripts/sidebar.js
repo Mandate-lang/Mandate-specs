7eeb5529f7deac2fa2df88bd0e38372fd452bb48
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-08-10 16:29:37 -0400

    Make sidebar properly render on nested pages as well

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,13 +1,54 @@
-async function load_sidebar()
-{
-    const file = await fetch("pages/sidebar.html");
-    const html = await file.text();
+// Data
+const pages = [
+  "preamble",
+];
 
-    //There should be only one body element
-    document.body.insertAdjacentHTML("afterbegin", html);
+//Helper Functions
+function set_caps(name)
+{
+  return String(name).charAt(0).toUpperCase() + String(name).slice(1);
 }
 
-document.addEventListener("DOMContentLoaded", function() 
+
+// Functions for adding sidebar
+async function load_sidebar()
 {
-  load_sidebar();
+  let file = null
+
+  //handle index.html being at top of directory
+  try
+  {
+    file = await fetch("../pages/sidebar.html");
+
+    if (!file.ok)
+    {
+      throw new Error("sidebar laoding failed with error code " + file.status);
+    }
+  }
+  catch(error)
+  {
+    file = await fetch("pages/sidebar.html");
+  }
+  const html = await file.text();
+
+  //There should be only one body element
+  document.body.insertAdjacentHTML("afterbegin", html);
+}
+async function setup_table_of_contents()
+{
+  const table_of_contents = await document.getElementById("table-of-contents");
+  for (const page of pages)
+  {
+    const link = "<li>\n" +
+                 "  <a href=\"pages\\" + page + ".html\">" + set_caps(page) +"</a>\n" +
+                 "</li>"
+    
+    table_of_contents.insertAdjacentHTML("beforeend", link);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", async function() 
+{
+  await load_sidebar();
+  await setup_table_of_contents();
 });
